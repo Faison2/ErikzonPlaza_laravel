a0f78cfb0c13ed6f448266663c9817d3fdeda843
Anesu Ngirande <anesu@dripdata.net>
2025-06-05 04:26:31 +0200

    add permission seeder to database defaults

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(PermissionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(WhyChooseUsTitleSeeder::class);
         \App\Models\Slider::factory(3)->create();
