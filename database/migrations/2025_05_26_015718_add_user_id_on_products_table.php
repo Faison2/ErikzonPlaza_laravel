99563a0385ef5730b8efc78209bddcc2b6e3bf1f
Anesu Ngirande <anesu@dripdata.net>
2025-06-05 04:22:04 +0200

    add migrations

---
+++ database/migrations/2025_05_26_015718_add_user_id_on_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('user_id')->after('id')->constrained();
+            $table->longText('seo_description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn(['user_id']);
+            $table->text('seo_description')->nullable()->change();
+        });
+    }
+};
